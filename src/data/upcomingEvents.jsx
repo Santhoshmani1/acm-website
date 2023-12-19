b69ebaa81df7f607eb55339adc5c63b4f62b8551
Santhosh Mani <pitakasanthosh@gmail.com>
2023-12-19 21:06:14 +0530

    Add upcoming event and update event page

---
+++ src/data/upcomingEvents.jsx
@@ -0,0 +1,17 @@
+import fullstack from "../assets/full-stack.jpg";
+
+const upcomingEvents = [
+    {
+      title: "Roadmap to Full stack development",
+      date: "December 23, 2023 Saturday",
+      startTime: "7:00 pm",
+      endTime: "8:00 PM",
+      description:
+        "",
+      venue: "Online",
+      registrationLink: "https://forms.gle/kHHMAQciLDmmgFGN8",
+      poster: fullstack,
+    },
+  ];
+
+  export default upcomingEvents;
--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,26 +1,13 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
-import vcs from "../../src/assets/version-control-logo.png";
-const upcomingEvents = [
-  {
-    title: "Introduction to Version Control with Git & Github",
-    date: "December 20, 2023",
-    startTime: "10:00 AM",
-    endTime: "12:00 PM",
-    description:
-      "Learn the essentials of version control using Git and GitHub. Join us for a hands-on experience with Git and GitHub! Learn how to use GitHub to collaborate with others on projects.",
-    venue: "Online, in-person",
-    registrationLink: "https://forms.gle/1Ln1y9JpbT9BpZt47",
-    poster: vcs,
-  },
-];
+import upcomingEvents from "../data/upcomingEvents";
 
 const Events = () => {
   return (
     <>
       <Header />
       <div className="events-page pt-20">
-        <h2 className="text-4xl p-2 m-2 font-semibold font-sans text-center">
+        <h2 className="text-2xl lg:text-3xl p-2 m-2 font-semibold font-sans text-center">
           Upcoming Events
         </h2>
         <div className="events-container mx-4">
@@ -29,16 +16,16 @@ const Events = () => {
               className="event-card max-w-screen-md mx-auto shadow-lg shadow-slate-500 p-4 m-4 rounded-xl"
               key={index}
             >
-              <div className="event-banner">
+              <div className="event-banner mx-auto">
                 <img
                   src={event.poster}
                   alt=""
-                  className="object-cover w-full"
-                  style={{ height: "300px" }}
+                  className=""
+                  style={{height:"auto",margin:"auto",maxHeight:"400px"}}
                 />
               </div>
               <div className="event-card-header">
-                <h3 className="text-2xl font-bold text-center p-2">
+                <h3 className="text-lg lg:text-xl font-bold text-center p-2">
                   {event.title}
                 </h3>
                 <div
