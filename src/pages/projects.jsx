a223d23be0b4901a18f920504506d737667e4677
Santhosh Mani <pitakasanthosh@gmail.com>
2023-12-24 17:40:12 +0530

    Update CSS styles for responsive coming soon image

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -9,8 +9,13 @@ const Projects = () => {
         <h2 className="text-4xl text-center p-3 font-semibold">Projects</h2>
       </div>
       <div className="upcoming-projects-container mb-40 mb:mb-0 lg:max-h-1/2">
-        <img src={comingSoon} alt="" id="coming-soon" />
+        <img src={comingSoon} alt="" id="coming-soon" className="h-3/4 w-5/6 sm:w-fit sm:h-fit "/>
+        <div className="maintenance-message">
+          <h3 className=" coming-soon-message font-bold text-2xl text-center">We are Under Maintenance</h3>
+          <h4 className="coming-soon-message text-center p-2 font-semibold">We will come back soon</h4>
+        </div>
       </div>
+
       <Footer />
     </>
   );
