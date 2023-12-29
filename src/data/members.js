581ab6c021e8bd2646acd123cdcd1c50e6691927
Santhosh <pitakasanthosh@gmail.com>
2023-12-29 19:01:23 +0530

    Refactor member component and update styles

--- src/data/members.js
+++ src/data/members.js
@@ -1,20 +1,20 @@
 import santhoshImg from "../assets/profilePictures/santhosh.jpg";
-import aryaImg from "../assets/profilePictures/arya.png"
-import tarunImg from "../assets/profilePictures/tarun.png"
-import chaitanyaImg from '../assets/profilePictures/chaitanya.png'
-import varshithImg from '../assets/profilePictures/varshith.png'
-import asreethImg from '../assets/profilePictures/asreeth.png'
-import vasutaImg from '../assets/profilePictures/vasuta.png'
-import anjanaImg from '../assets/profilePictures/anjana.png'
+import aryaImg from "../assets/profilePictures/arya.png";
+import tarunImg from "../assets/profilePictures/tarun.png";
+import chaitanyaImg from "../assets/profilePictures/chaitanya.png";
+import varshithImg from "../assets/profilePictures/varshith.png";
+import asreethImg from "../assets/profilePictures/asreeth.png";
+import vasutaImg from "../assets/profilePictures/vasuta.png";
+import anjanaImg from "../assets/profilePictures/anjana.png";
 
-const members = [
+export const ExecutiveTeam = [
   {
     name: "Tarun Varaha sai",
     profilePicture: tarunImg,
     role: "Chair Person",
     department: "Computer Science and Engineering",
     github: "",
-    linkedin: "tarunsai08",
+    linkedin: "https://linkedin.com/in/tarunsai08",
     twitter: "",
   },
   {
@@ -23,16 +23,7 @@ const members = [
     role: "Vice Chair",
     department: "Computer Science and Engineering",
     github: "",
-    linkedin: "",
-    twitter: "",
-  },
-  {
-    name: "Chaitanya D",
-    profilePicture: chaitanyaImg,
-    role: "Secretary",
-    department: "Computer Science and Engineering",
-    github: "",
-    linkedin: "chaitanya-dhavaleswarapu-31047b259",
+    linkedin: "https://linkedin.com/in/",
     twitter: "",
   },
   {
@@ -41,34 +32,19 @@ const members = [
     role: "Membership Chair",
     department: "Computer Science and Engineering",
     github: "",
-    linkedin: "asreeth-putheti-a60680251",
+    linkedin: "https://linkedin.com/in/asreeth-putheti-a60680251",
     twitter: "",
   },
+];
+
+export const AssociateTeam = [
   {
-    name: "Vasuta Kuchhadia",
-    profilePicture: vasutaImg,
-    role: "Alumni manager & Marketing Head",
-    department: "Information Technology",
-    github: "",
-    linkedin: "",
-    twitter: "",
-  },
-  {
-    name: "Anjana Manaswitha",
-    profilePicture: anjanaImg,
-    role: "Content writer",
-    department: "Information Technology",
-    github: "",
-    linkedin: "chada-sai-anjana-manaswitha-906805259",
-    twitter: "",
-  },
-  {
-    name: "Siddhartha V",
-    profilePicture: "",
-    role: "Lead Android developer",
+    name: "Chaitanya D",
+    profilePicture: chaitanyaImg,
+    role: "Secretary",
     department: "Computer Science and Engineering",
     github: "",
-    linkedin: "vanka-siddhartha-35b767229",
+    linkedin: "https://linkedin.com/in/chaitanya-dhavaleswarapu-31047b259",
     twitter: "",
   },
   {
@@ -77,19 +53,50 @@ const members = [
     role: "Treasurer",
     department: "Computer Science and Engineering",
     github: "",
-    linkedin: "arya-bodda-03700a217",
+    linkedin: "https://linkedin.com/in/arya-bodda-03700a217",
+    twitter: "",
+  },
+];
+
+export const MarketingTeam = [
+  {
+    name: "Vasuta Kuchhadia",
+    profilePicture: vasutaImg,
+    role: "Alumni manager & Marketing Head",
+    department: "Information Technology",
+    github: "",
+    linkedin: "https://linkedin.com/in/vasuta-kuchhadia27062003",
+    twitter: "",
+  },
+  {
+    name: "Anjana Manaswitha",
+    profilePicture: anjanaImg,
+    role: "Content writer",
+    department: "Information Technology",
+    github: "",
+    linkedin: "https://linkedin.com/in/chada-sai-anjana-manaswitha-906805259",
+    twitter: "",
+  },
+];
+
+export const developmentTeam = [
+  {
+    name: "Siddhartha V",
+    profilePicture: "",
+    role: "Lead Android developer",
+    department: "Computer Science and Engineering",
+    github: "",
+    linkedin: "https://linkedin.com/in/vanka-siddhartha-35b767229",
     twitter: "",
   },
   {
     name: "Santhosh Mani",
-    profilePicture:
-      santhoshImg,
+    profilePicture: santhoshImg,
     role: "Lead Web developer",
     department: "Electronics & Communications",
-    github: "Santhoshmani1",
-    linkedin: "santhoshmanip",
-    twitter: "SanthoshMani_P",
+    github: "https://www.github.com/Santhoshmani1",
+    linkedin: "https://www.linkedin.com/in/santhoshmanip",
+    twitter: "https://twitter.com/SanthoshMani_P",
   },
 ];
 
-export default members;
--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -1,10 +1,7 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Animate from "../components/AnimateComponent";
-import members from "../data/members";
-import { Link } from "react-router-dom";
-import { FaGithub, FaLinkedin } from "react-icons/fa6";
-import { FaXTwitter } from "react-icons/fa6";
+import {ExecutiveTeam,AssociateTeam,MarketingTeam,developmentTeam} from "../data/members";
+import Member from "../components/member";
 
 const Team = () => {
   return (
@@ -22,74 +19,34 @@ const Team = () => {
           Meet the Team
         </h2>
 
-        <div className="members-container lg:grid lg:grid-cols-2 lg:gap-2">
-          {members.map((member, index) => (
-            <div key={index} className="my-5 mx-5">
-              <Animate>
-                <div className="member-card max-w-4/5 flex flex-col justify-evenly items-center max-w-md mx-auto p-4 m-2 border shadow-sm rounded-xl  bg-white  shadow-blue-500">
-                  <div className="member-card-header">
-                    {member.profilePicture ? (
-                      <img
-                        src={member.profilePicture}
-                        alt={member.name}
-                        className="rounded-2xl p-2 w-1/2 mx-auto h-auto"
-                      />
-                    ) : (
-                      <>
-                        <div className="h-40 w-10 px-20 bg-blue-500 text-white text-center text-4xl mt-10 rounded-xl flex justify-center items-center">
-                          <div className="text-5xl">{member.name[0]}</div>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                  <div className="member-card-body">
-                    <h3 className="member-name text-center text-2xl font-semibold p-1">
-                      {member.name}
-                    </h3>
-                    <h4 className="member-role text-lg text-center">
-                      {member.role}
-                    </h4>
-                    <h4 className="member-year">{member.year}</h4>
-                    <h4
-                      className="member-department text-center"
-                      style={{ color: "#5f6368" }}
-                    >
-                      Department of {member.department}
-                    </h4>
-                    <div className="member-socials">
-                      <div className="flex justify-evenly p-2 items-center">
-                        
-                          <Link to={member.github}>
-                            <FaGithub
-                              className=""
-                              style={{ fontSize: "1.7rem" }}
-                            />
-                          </Link>
-                      
-                          <Link
-                            to={ member.linkedin}
-                          >
-                            <FaLinkedin
-                              style={{ fontSize: "1.7rem", color: "#069" }}
-                            />
-                          </Link>
-                        
-                          <Link to={member.twitter}>
-                            <FaXTwitter className="rounded-xl p-1"
-                              style={{ fontSize: "1.7rem", color: "white",backgroundColor:"black" }}
-                            />
-                          </Link>
-                        
-                      </div>
-                      <div></div>
-                      <div></div>
-                    </div>
-                  </div>
-                </div>
-              </Animate>
-            </div>
+          <h3 className="p-2 text-xl font-bold text-center leading-loose" style={{fontFamily:"Montserrat"}}>Executive Team</h3>
+        <div className="executives-container lg:grid lg:grid-cols-3 lg:gap-2 flex flex-col justify-center items-center">
+          {ExecutiveTeam.map((member,index) => (
+            <Member member={member} key={index}/>
           ))}
         </div>
+
+        <h3 className="p-2 text-xl font-bold text-center leading-loose" style={{fontFamily:"Montserrat"}}>Associate Team</h3>
+        <div className="associates-container flex flex-col lg:flex-row justify-evenly items-center mx-auto">
+          {AssociateTeam.map((member,index) => (
+            <Member member={member} key={index}/>
+          ))}
+          </div>
+
+          <h3 className="p-2 text-xl font-bold text-center leading-loose" style={{fontFamily:"Montserrat"}}>Marketing Team</h3>
+        <div className="marketing-container flex flex-col lg:flex-row justify-evenly items-center mx-auto">
+          {MarketingTeam.map((member,index) => (
+            <Member member={member} key={index}/>
+          ))}
+          </div>
+
+          <h3 className="p-2 text-xl font-bold text-center leading-loose" style={{fontFamily:"Montserrat"}}>Development Team</h3>
+        <div className="development-container flex flex-col lg:flex-row justify-evenly items-center mx-auto">
+          {developmentTeam.map((member,index) => (
+            <Member member={member} key={index}/>
+          ))}
+          </div>
+
       </div>
       <Footer />
     </>
